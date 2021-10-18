02bd9492e7777ad334ef968aae3246dbfdb8eaf5
radenjaelaniakbar <91249966+radenjaelaniakbar@users.noreply.github.com>
2021-10-18 01:40:14 -0700

    Add files via upload

---
+++ Method.java
@@ -0,0 +1,14 @@
+public class Method {
+
+    static void myMethod(String name, int value) {
+        System.out.println(name + " is " + value);
+    }
+
+    public static void main(String[] args) {
+        myMethod("yoga", 80);
+        myMethod("Dani", 60);
+        myMethod("Pratama", 90);
+        myMethod("Buya", 77);
+        myMethod("Sri", 75);
+    }
+}
